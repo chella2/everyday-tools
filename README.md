c09156ad4119aa609e90c30aa4fd310d1d3ba17f
Chella <chella@Christians-MacBook-Pro.local>
2021-01-20 20:53:39 +0100

    Tool to collect photos in a directory

--- README.md
+++ README.md
@@ -17,8 +17,25 @@ Read ov2 files by GPS manufactor TomTom and outputs as CSV (Comma Separated Valu
 Create ov2-file from a CSV-file. Each row in the input CSV must be a location using the format 'longitude,latitude,"Name"'. The longitude and latitude must be in WGS84 coordinates using decimal degrees (DD).
 
 
+## photo_collector.py
+Recursivly collect photos and images from a source path to target path. Only copies photos that are not already at target directory. Images that are coopied from the source are sorted in target directory using structure Year/Month. The image date is based on Exif metadata (DateTimeOriginal) if available, otherwise most recent metadata change (Unix) or time of creation (Windows).
+
+This script requires package exifread. See 'Setup virtual environment'
+
 
 # Related information
 
 ## Creating POI set for TomTom GPS
 Instructions making a POI set https://www.tomtom.com/lib/doc/TomTomTips/index.html?poi_set_using_makeov2.htm
+
+
+# Setup virtual environment
+
+* Create a virtual python environment called 'env'
+    virtualenv env
+
+* Activate the virtual environment
+    source env/bin/activate
+
+* Install required packages
+    pip install -r requirements.txt
---
+++ photo_collector.py
@@ -0,0 +1,125 @@
+''' Recursivly collect photos and images from a source path to target path. 
+
+Note the following:
+* Only images that are not already in the target directory are copied.
+* Images copied to target will be sorted in directory structure: Year/Month.
+
+'''
+
+import os
+import shutil
+import mimetypes
+import re
+import hashlib
+import pathlib
+from datetime import datetime
+import argparse
+from pprint import pprint
+
+
+import exifread
+""" Some python EXIF packages
+* PIL - old, not maintained anymore
+* Pillow - PIL fork, libraries in c/c++
+* exif  -
+* exifread (repo name: exif-py)
+* py3exiv2
+* pyexiv2
+"""
+
+
+
+def image_files(path):
+    ''' Returns a dictionary with image files at given path'''
+
+    file_dict = dict()
+    
+    img_regex = re.compile(r'^image/')
+    
+    
+    for root, dir, files in os.walk(path):
+    
+        for filename in files:
+    
+            file_path = os.path.join(root, filename) 
+            file_mime = mimetypes.guess_type(file_path)
+            
+
+            if re.match(img_regex, file_mime[0] or ''):
+    
+                with open(file_path, 'rb') as f:
+
+                    file_chksum = hashlib.md5(f.read()).hexdigest()
+                    tags = exifread.process_file(f, stop_tag='EXIF DateTimeOriginal')
+    
+    
+                if 'EXIF DateTimeOriginal' in tags:
+                    create_date = datetime.strptime(tags['EXIF DateTimeOriginal'].values,
+                                                    '%Y:%m:%d %H:%M:%S')
+    
+                else:
+                    fn = pathlib.Path(file_path)
+                    create_date = datetime.fromtimestamp(fn.stat().st_ctime)
+    
+    
+                file_dict[file_chksum] = {'path': file_path, 'date': create_date }
+    
+    return file_dict
+
+
+
+
+# Input argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument('source_dir', help='Source directory path')
+parser.add_argument('target_dir', help='Target directory path')
+parser.add_argument('-f', '--force', help='Do not prompt for confirmation', action='store_true')
+parser.add_argument('-v', '--verbose', help='Enable verbosity', action='store_true')
+args = parser.parse_args()
+
+
+
+source_files = image_files(args.source_dir)
+
+target_files = image_files(args.target_dir)
+
+if args.verbose:
+    print('{}Files found at source path: {}'.format(os.linesep, args.source_dir))
+    pprint(source_files)
+    print('{}Files found at target path: {}'.format(os.linesep, args.target_dir))
+    pprint(target_files)
+
+
+print('{} images files found in {}'.format(len(source_files), args.source_dir))
+print('{} images files found in {}'.format(len(target_files), args.target_dir))
+
+
+if not args.force: 
+    confirm_copy = input('Copy missing files to target? yes/no: ')
+
+    if not confirm_copy == 'yes':
+        raise SystemExit('Exits. You answer differ from yes')
+
+
+copy_counter = 0
+
+for chk_sum, img in source_files.items():
+
+    if not chk_sum in target_files:
+
+        year_taken= img['date'].strftime('%Y')
+        month_taken= img['date'].strftime('%m_%B')
+
+        target_path = os.path.join( args.target_dir,
+                                    year_taken,
+                                    month_taken, 
+                                    os.path.basename(img['path']))
+        
+        os.makedirs(os.path.dirname(target_path), exist_ok=True)
+        shutil.copyfile(img['path'], target_path, follow_symlinks=False)
+        shutil.copystat(img['path'], target_path, follow_symlinks=False)
+
+        copy_counter += 1
+
+
+print('{} files copied to {}'.format(copy_counter, args.target_dir))
