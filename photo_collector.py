cbf9cbb89996ab5a0f9a67c266e69cee657207ed
Chella <chella@Christians-MacBook-Pro.local>
2021-01-26 22:21:42 +0100

    Various improvments of photo_collector
    * Improved error when getting image exif data
    * Create log file at target dir
    * Handle the case when target directory already has a file with the
      same name

--- photo_collector.py
+++ photo_collector.py
@@ -4,6 +4,8 @@ Note the following:
 * Only images that are not already in the target directory are copied.
 * Images copied to target will be sorted in directory structure: Year/Month.
 
+Room for improvements:
+* Status information while copying images to target
 '''
 
 import os
@@ -14,7 +16,6 @@ import hashlib
 import pathlib
 from datetime import datetime
 import argparse
-from pprint import pprint
 
 
 import exifread
@@ -29,10 +30,10 @@ import exifread
 
 
 
-def image_files(path):
-    ''' Returns a dictionary with image files at given path'''
+def find_images(path, verbose=False):
+    ''' Returns a dict with image files at input path'''
 
-    file_dict = dict()
+    path_content = dict()
 
     img_regex = re.compile(r'^image/')
 
@@ -47,24 +48,45 @@ def image_files(path):
 
             if re.match(img_regex, file_mime[0] or ''):
 
-                with open(file_path, 'rb') as f:
+                with open(file_path, 'rb') as fp:
 
-                    file_chksum = hashlib.md5(f.read()).hexdigest()
-                    tags = exifread.process_file(f, stop_tag='EXIF DateTimeOriginal')
+                    file_chksum = hashlib.md5(fp.read()).hexdigest()
+
+                    try:
+                        tags = exifread.process_file(fp, stop_tag='EXIF DateTimeOriginal')
+
+                    except:
+                        tags = {}
+
+                        if verbose:
+                            print('exiftool failed on file {}'.format(file_path))
 
 
-                if 'EXIF DateTimeOriginal' in tags:
-                    create_date = datetime.strptime(tags['EXIF DateTimeOriginal'].values,
+
+                try:
+                    creation_date = datetime.strptime(tags['EXIF DateTimeOriginal'].values,
                                                     '%Y:%m:%d %H:%M:%S')
 
-                else:
-                    fn = pathlib.Path(file_path)
-                    create_date = datetime.fromtimestamp(fn.stat().st_ctime)
+                except:
+
+                    try:
+                        date_match = re.match(r'^(?P<year>\d+)[:/](?P<month>\d+)[:/](?P<day>\d+)',
+                                              tags['EXIF DateTimeOriginal'].values)
+
+                        date_dict = date_match.groupdict()
+
+                        creation_date = datetime(int(date_dict['year']), int(date_dict['month']), int(date_dict['day']))
 
 
-                file_dict[file_chksum] = {'path': file_path, 'date': create_date }
+                    except:
+                        fn = pathlib.Path(file_path)
+                        creation_date = datetime.fromtimestamp(fn.stat().st_mtime)
 
-    return file_dict
+
+                path_content[file_chksum] = {'path': file_path, 'date': creation_date }
+
+
+    return path_content
 
 
 
@@ -75,23 +97,18 @@ parser.add_argument('source_dir', help='Source directory path')
 parser.add_argument('target_dir', help='Target directory path')
 parser.add_argument('-f', '--force', help='Do not prompt for confirmation', action='store_true')
 parser.add_argument('-v', '--verbose', help='Enable verbosity', action='store_true')
+
 args = parser.parse_args()
 
 
+print('Searching for images. This may take a while...')
 
-source_files = image_files(args.source_dir)
-
-target_files = image_files(args.target_dir)
-
-if args.verbose:
-    print('{}Files found at source path: {}'.format(os.linesep, args.source_dir))
-    pprint(source_files)
-    print('{}Files found at target path: {}'.format(os.linesep, args.target_dir))
-    pprint(target_files)
+source_files = find_images(args.source_dir, args.verbose)
+target_files = find_images(args.target_dir, args.verbose)
 
 
-print('{} images files found in {}'.format(len(source_files), args.source_dir))
-print('{} images files found in {}'.format(len(target_files), args.target_dir))
+print('{} images files found in source directory {}'.format(len(source_files), args.source_dir))
+print('{} images files found in target directory {}'.format(len(target_files), args.target_dir))
 
 
 if not args.force:
@@ -115,6 +132,16 @@ for chk_sum, img in source_files.items():
                                     month_taken,
                                     os.path.basename(img['path']))
 
+
+        # ensure target file does not already exists
+        img_root, img_ext = os.path.splitext(target_path)
+        n = 0
+
+        while os.path.exists(target_path):
+            n += 1
+            target_path = ''.join([img_root, '_' + str(n), img_ext])
+
+
         os.makedirs(os.path.dirname(target_path), exist_ok=True)
         shutil.copyfile(img['path'], target_path, follow_symlinks=False)
         shutil.copystat(img['path'], target_path, follow_symlinks=False)
@@ -123,3 +150,9 @@ for chk_sum, img in source_files.items():
 
 
 print('{} files copied to {}'.format(copy_counter, args.target_dir))
+
+
+with open(os.path.join(args.target_dir, 'photo_collector_log.txt'), 'a') as fp:
+
+    for sha in target_files:
+        fp.write(sha + '\n')
